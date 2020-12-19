08bd162a3de2d8b74c8bfb421b1de5a2a1e0d333
coolthx <raraga@gmail.com>
2020-12-19 14:41:42 -0800

    Mapping from teams works; Building TeamID handler

--- src/App.js
+++ src/App.js
@@ -1,30 +1,51 @@
-import React from 'react';
-import nba from 'nba';
+import React from "react";
+import nba from "nba";
 
 class App extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      team: [] 
-    }
+      teams: [],
+      warriors: [],
+      currentTeamId: "",
+      showId: false,
+    };
   }
   componentDidMount() {
-    let w = nba.players.filter(player => player.teamId === 1610612744);
-    let warriors = w.map(warrior => warrior)
-    
+    // Filtering all players that play for the Warriors
+    let w = nba.players.filter((player) => player.teamId === 1610612744);
+    let teamData = nba.teams;
+
     this.setState({
-      team: warriors
-    })
+      teams: teamData,
+      warriors: w,
+    });
   }
+  showTeamId() {}
   render() {
     return (
       <div className="App">
         <h1>NBA API App</h1>
         <h2>2019 / 2020 Warriors Lineup</h2>
         <ul>
-          {this.state.team.map(
-            t => <li key={t.playerId} value={t.playerId}>{t.firstName} {t.lastName}</li>
-          )}
+          {this.state.warriors.map((w) => (
+            <li key={w.playerId} value={w.playerId}>
+              {w.firstName} {w.lastName}
+            </li>
+          ))}
+        </ul>
+        <h2>Teams</h2>
+        <ul>
+          <li>
+            {this.state.teams.map((t) => (
+              <li value={t.teamId}>
+                {t.teamId}{" "}
+                <a href="" onclick={this.showTeamId}>
+                  {t.teamName}
+                </a>
+              </li>
+            ))}
+          </li>
         </ul>
       </div>
     );
